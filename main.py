c49063c62886b058d59e642e87fc95377e7625ab
Malachi <malachithorne@gmail.com>
2022-03-02 21:15:12 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 import kivy 
 from kivy.app import App
 from kivy.uix.label import Label
@@ -8,18 +10,25 @@ from kivy.uix.textinput import TextInput
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
+        
+        self.inside = GridLayout()
+        self.inside.cols = 2
+        
         self.cols = 2
-        self.add_widget(Label(text="First Name: "))
+        self.inside.add_widget(Label(text="First Name: "))
         self.name = TextInput(multiline = False)
-        self.add_widget(self.name)
+        self.inside.add_widget(self.name)
         
-        self.add_widget(Label(text="Last Name: "))
+        self.inside.add_widget(Label(text="Last Name: "))
         self.lastName = TextInput(multiline = False)
-        self.add_widget(self.lastName)
+        self.inside.add_widget(self.lastName)
         
-        self.add_widget(Label(text="Email: "))
+        self.inside.add_widget(Label(text="Email: "))
         self.email = TextInput(multiline = False)
-        self.add_widget(self.email)
+        self.inside.add_widget(self.email)
+        
+        self.submit = Button(text= "Submit", font_size=40)
+        self.add_widget(self.submit)
 
 class MyApp(App):
     def build(self):
